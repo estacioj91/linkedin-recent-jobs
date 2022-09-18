b9d888bbd8e3926c9277bda375ce1cfdc51c2b91
John Estacio <john.estacio@workato.com>
2022-09-18 11:42:21 -0700

    data finished

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-const data = {
+const DATA = {
   jobs: [
     {
       name: "Google",
-      logo: "https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_100_100/0/1519856215226?e=1671667200&v=beta&t=DbbOvW-gTD68zgGNE4put9sjoR8bBBKK0qfc0rE-UhcΩ"
-
+      logo: "https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_100_100/0/1519856215226?e=1671667200&v=beta&t=DbbOvW-gTD68zgGNE4put9sjoR8bBBKK0qfc0rE-UhcΩ",
+      job_title: "Frontend Engineer, Growth Team",
+      location: "San Jose, CA",
+      top_applicant: false,
+      posted: "2 weeks ago"
     },
-    {},
-    {}
+    {
+      name: "Google",
+      logo: "https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_100_100/0/1519856215226?e=1671667200&v=beta&t=DbbOvW-gTD68zgGNE4put9sjoR8bBBKK0qfc0rE-UhcΩ",
+      job_title: "Frontend Engineer, Growth Team",
+      location: "San Jose, CA",
+      top_applicant: false,
+      posted: "2 weeks ago"
+    },
+    {
+      name: "Google",
+      logo: "https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_100_100/0/1519856215226?e=1671667200&v=beta&t=DbbOvW-gTD68zgGNE4put9sjoR8bBBKK0qfc0rE-UhcΩ",
+      job_title: "Frontend Engineer, Growth Team",
+      location: "San Jose, CA",
+      top_applicant: false,
+      posted: "2 weeks ago"
+    }
   ]
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
