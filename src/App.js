77d4d9211c44f8fde3b307696c36497bd1a814a5
John Estacio <john.estacio@workato.com>
2022-10-03 14:26:03 -0700

    context example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import UserContext from './UserContext';
+import { useContext } from 'react';
+import { useState } from 'react';
+
+function One(){
+  const {count, setCount} = useContext(UserContext);
+
+  return (
+    <button onClick={() => {
+      setCount(count+1)
+    }}>+</button>
+  )
+}
+function Two(){
+  const {count, setCount} = useContext(UserContext);
+  return (
+    <div className="">{count}</div>
+  )
+}
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <UserContext.Provider value={{count, setCount}}>
+        <One></One>
+        <Two></Two>
+      </UserContext.Provider>
     </div>
   );
 }
